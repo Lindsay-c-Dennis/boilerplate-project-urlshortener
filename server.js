c4418b43d0cbf104d136637d2d4b2e17f2511040
Lindsay Dennis <30577902+Lindsay-c-Dennis@users.noreply.github.com>
2019-05-10 13:48:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const dns = require('dns')
 var cors = require('cors');
 
 var app = express();
@@ -14,8 +14,21 @@ var port = process.env.PORT || 3000;
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
 
-app.use(cors());
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
+mongoose.Promise = require('bluebird');
+
+const db = mongoose.connection
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String
+})
+const ShortUrl = mongoose.model('ShortUrl', urlSchema)
+
+app.use(cors());
+app.use(bodyParser.json());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
@@ -30,6 +43,41 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.post("/api/shorturl/new", bodyParser.urlencoded({extended:false}), (req, res) => {
+ let url = req.body.url.replace(/https?:\/\//, "");
+ dns.lookup(url, (err, add, fam) => {
+    if (err) {
+      res.json({"error": "invalid url"});
+    } else  {
+      let num = Math.ceil(Math.random()*1000);
+      const obj = new ShortUrl({original_url: req.body.url, short_url:String(num)})
+      obj.save((err,data) => {
+        if (err) { res.send("Error saving to database")}
+      })
+      res.json({original_url: req.body.url, short_url:String(num)})
+      
+      
+  }
+  })
+  
+})
+
+app.get("/api/shorturl/:id", (req,res) => {
+  ShortUrl.findOne({short_url: req.params.id}, (err, data) => {
+    if (data) {
+      let url = data.original_url
+      let newUrl = url.match(/https?:\/\//) ? url : "https://" + url
+      res.redirect(newUrl);
+    } else {
+      res.send("URL not found")
+      
+    }
+  })
+});
+ 
+  
+
 
 
 app.listen(port, function () {
